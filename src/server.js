48a74f17858ec1c4a88fbf596bfca412cfc9aa14
annafiochi <annafiochi.tds2.senai@gmail.com>
2024-08-29 11:49:37 -0300

    feat: start to refactor api routes

--- src/server.js
+++ src/server.js
@@ -5,45 +5,18 @@ config()
 const serverPort = process.env.PORT ||3000
 const app = express()
 app.use(express.json())
-const emocoes = [
-    {
-        id:1, 
-        nome:'Nojinho',
-        cor:'Verde'
-    },
-    {
-        id:2, 
-        nome:'Alegria',
-        cor:'Amarelo'
-    },
-    {
-        id:3, 
-        nome:'Raiva',
-        cor:'Vermelho'
-    }
-]
+
 app.get('/', (req, res) => {
     return res.status(200).send({message: 'hello,  world!'})
 })
 app.get('/2tds2', (req, res) => {
     return res.status(200).send({message: 'hello,  world!'})
 })
-app.get('/emocoes', (req, res) => {
-    return res.status(200).send(emocoes)
-})
+
 app.get('/personagens', (req, res) => {
     return res.status(200).send(personagens)
 })
-app.post('/emocoes', (req, res) => {
-    const{ nome, cor} = req.body
-    const newEmotion = {
-        id:emocoes.length +1,
-        nome:nome,
-        cor:cor
-    }
-    emocoes.push(newEmotion)
-    return res.status(200).send(emocoes)
-})
+
 const personagens = [
     {
         id:100,
