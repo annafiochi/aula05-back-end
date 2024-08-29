be87f1cbe45547f42057c9a437abe27e85a74ca4
annafiochi <annafiochi.tds2.senai@gmail.com>
2024-08-29 10:49:56 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -5,12 +5,52 @@ config()
 const serverPort = process.env.PORT ||3000
 const app = express()
 app.use(express.json())
+const emocoes = [
+    {
+        id:1, 
+        nome:'Nojinho',
+        cor:'Verde'
+    },
+    {
+        id:2, 
+        nome:'Alegria',
+        cor:'Amarelo'
+    },
+    {
+        id:3, 
+        nome:'Raiva',
+        cor:'Vermelho'
+    }
+]
 app.get('/', (req, res) => {
     return res.status(200).send({message: 'hello,  world!'})
 })
 app.get('/2tds2', (req, res) => {
-    return res.status(500).send({message: 'hello,  world!'})
+    return res.status(200).send({message: 'hello,  world!'})
 })
+app.get('/emocoes', (req, res) => {
+    return res.status(200).send(emocoes)
+})
+app.get('/personagens', (req, res) => {
+    return res.status(200).send(personagens)
+})
+const personagens = [
+    {
+        id:100,
+        nome:'Totoro',
+        vivo: true
+    },
+    {
+        id:101,
+        nome:'Elsa',
+        vivo: true
+    },
+    {
+        id:102,
+        nome:'Sr. batata',
+        vivo: true
+    }
+]
 app.listen(serverPort, () => {
     console.log(`🎉 server started on http://localhost:${serverPort}`)
 })
