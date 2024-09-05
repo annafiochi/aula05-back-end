ad6621022de317d33e2bf9837337c57a3e8876d2
annafiochi <annafiochi.tds2.senai@gmail.com>
2024-09-05 17:19:11 -0300

    feat: create get by id method

--- src/routes/emocoes.routes.js
+++ src/routes/emocoes.routes.js
@@ -17,10 +17,12 @@ let emocoes = [
         nome:'Raiva',
         cor:'Vermelho'
     }
-]
+];
+// rota para buscar todas as emoções
 emocoesRoutes.get('/', (req, res) => {
     return res.status(200).send(emocoes)
-})
+});
+//rota para criar uma nova emoção
 emocoesRoutes.post('/', (req, res) => {
     const{ nome, cor} = req.body
     const newEmotion = {
@@ -30,6 +32,25 @@ emocoesRoutes.post('/', (req, res) => {
     }
     emocoes.push(newEmotion)
     return res.status(201).send(emocoes)
-})
+});
+
+//rota para buscar emocoes por id
+emocoesRoutes.get("/:id", (req,res) => {
+    const {id}= req.params;
+  //  console.log (id);
+    const emocao = emocoes.find((emotion) => emotion.id == id )
+    
+    if (!emocao){
+        return res.status(404).send({
+            message: "emoção não encontrada!",
+        });
+    }
+
+    return res.status(200).send({
+        message: "emoção encontrada",
+        emocao,
+    });
+});
+
 export default emocoesRoutes 
     
